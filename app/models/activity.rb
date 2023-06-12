a30c20ee48a8d18b6e6a7d90940e66429714c158
KevinJSS <kevin.alvarado@ucrso.info>
2023-06-11 19:59:59 -0600

    Fixed the delete button error and added date model validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,13 @@ class Activity < ApplicationRecord
     #validations
     validates :worked_hours, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 24 }
     validates :date, presence: true
+    validate :date_lower_than_or_equal_to_today
+
+    def date_lower_than_or_equal_to_today
+        if date.present? && date > Date.today
+            errors.add(:date, "La fecha no puede ser mayor al día de hoy")
+        end
+    end
 
     #associations
     belongs_to :user
