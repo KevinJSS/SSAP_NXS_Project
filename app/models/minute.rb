18b997b66e9d0a1a1c61ba7ee6a7e1cc4707ce15
KevinJSS <kevin.alvarado@ucrso.info>
2023-06-21 10:55:27 -0600

    Add minutes model validations for duplicate attendees

--- app/models/minute.rb
+++ app/models/minute.rb
@@ -5,6 +5,8 @@ class Minute < ApplicationRecord
     validates :start_time, presence: true
     validates :end_time, presence: true
     validate :start_time_greater_than_end_time
+    validate :validate_attendees
+    validate :duplicate_attendees
 
     def start_time_greater_than_end_time
         if start_time.present? && end_time.present? && end_time <= start_time
@@ -15,6 +17,21 @@ class Minute < ApplicationRecord
     def get_meeting_duration
         duration = (self.end_time - self.start_time) / 1.hour
         duration.round(2) 
+    end
+
+    def validate_attendees
+        if self.minutes_users.empty?
+            errors.add(:minutes_users, "es necesario seleccionar al menos un asistente")
+        end
+    end
+
+    def duplicate_attendees
+        nested_attributes = self.minutes_users
+        user_ids = nested_attributes.map(&:user_id)
+        duplicates = user_ids.select { |id| user_ids.count(id) > 1 }.uniq
+        if !duplicates.empty?
+            errors.add(:minutes_users, "no puede haber asistentes duplicados")
+        end
     end
 
     #associations
