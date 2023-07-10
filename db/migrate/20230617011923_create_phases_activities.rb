04e98bee87a58af5c34cf87f813505a5236075e6
KevinJSS <kevin.alvarado@ucrso.info>
2023-07-09 23:24:38 -0600

    Production changes

--- db/migrate/20230617011923_create_phases_activities.rb
+++ db/migrate/20230617011923_create_phases_activities.rb
@@ -3,7 +3,6 @@ class CreatePhasesActivities < ActiveRecord::Migration[7.0]
     create_table :phases_activities do |t|
       t.references :phase, null: false, foreign_key: true
       t.references :activity, null: false, foreign_key: true
-      t.integer :hours, :integer, null: false, default: 0
 
       t.timestamps
     end
