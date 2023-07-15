99527d3410a60d73c33be96c9e91ae254ada32a2
KevinJSS <kevin.alvarado@ucrso.info>
2023-07-15 15:57:38 -0600

    Add documentation to homepage views, scss and js

--- app/javascript/controllers/clock_controller.js
+++ app/javascript/controllers/clock_controller.js
@@ -1,14 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Clock Controller
 // Connects to data-controller="clock"
 export default class extends Controller {
+  // The static targets attribute declares an array of targets defined in the HTML template that this controller will interact with.
   static targets = ['time'];
 
+  // The connect method is called when the controller is connected to an element. It sets up the initial time display and schedules periodic updates.
   connect() {
     this.updateTime();
     setInterval(() => this.updateTime(), 1000);
   }
 
+  // The updateTime method retrieves the current time and formats it. It then updates the clock display element with the formatted time.
   updateTime() {
     const clock = document.getElementById('clock');
 
@@ -21,6 +25,7 @@ export default class extends Controller {
     }
   }
 
+  // The formatTime method takes the hours and minutes as parameters and formats them into a 12-hour format with AM/PM indicator.
   formatTime(hours, minutes) {
     const amPm = hours >= 12 ? 'PM' : 'AM';
     const twelveHourFormat = hours % 12 || 12;
