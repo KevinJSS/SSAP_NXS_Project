21be452ae6af2999e13dd1320abe405ea747b851
KevinJSS <kevin.alvarado@ucrso.info>
2023-07-15 23:39:54 -0600

    Added documentation to all the assets (scss and js files)

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -5,6 +5,8 @@ export default class extends Controller {
   connect() {
   }
 
+  // Called when the "back" button is clicked
+  // So we can go back in the browser history
   goBack() {
     window.history.back()
   }
--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -4,15 +4,31 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["link", "subtitle"];
 
+  /**
+   * The connect method is called when the controller is connected.
+   * It sets the active link based on the current view and adds a resize event listener to trigger the setActiveLink method.
+   */
   connect() {
     this.setActiveLink();
     window.addEventListener("resize", this.onResize.bind(this));
   }
 
+  /**
+   * The onResize method is called when the window is resized.
+   * It triggers the setActiveLink method to update the active link based on the current view.
+   */
   onResize() {
     this.setActiveLink();
   }
 
+   /**
+   * The setActiveLink method updates the active link in the sidebar based on the current view.
+   * It removes the "active" class from all link elements and their parent elements.
+   * Then it finds the active section based on the data-section-id attribute of the subtitle elements,
+   * and the active link based on the data-link-id attribute of the link elements.
+   * If an active link is found, it adds the "active" class to its parent element and the link element.
+   * If an active section is found, it adds the "active" class to the section element.
+   */
   setActiveLink() {
     const main = document.querySelector("main");
 
