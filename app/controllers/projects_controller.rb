29503c77b2165a45f32f0b43fbe2a770d942d2e5
KevinJSS <kevin.alvarado@ucrso.info>
2023-07-15 18:37:50 -0600

    Added documentation for the project controller and model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,38 @@
 class ProjectsController < ApplicationController
+  # The `before_action` callback method ensures that the user is authenticated
+  # before accessing any action in this controller. It helps enforce the requirement
+  # for a user to be logged in before interacting with the `ProjectsController`.
   before_action :authenticate_user!
+
+  # The `before_action` callback method `set_project` is called before the `show`, `edit`, `update`,
+  # and `destroy` actions. It sets the `@project` instance variable by finding the project based on the
+  # provided `id` parameter. If the project is not found, it redirects to the projects index page with an alert.
   before_action :set_project, only: %i[ show edit update destroy ]
+
+  # The `before_action` callback method `set_admins` is called before the `index`, `new`, and `edit` actions.
+  # It sets the `@admins` and `@all_admins` instance variables by finding all the users with the role of
+  # `admin` and `collaborator` respectively. If no users are found, it sets the instance variables to `nil`.
   before_action :set_admins
+
+  # The `before_action` callback method `get_change_log` is called before the `show`, `edit`, and `update` actions.
+  # It sets the `@project_change_log` instance variable by finding all the change logs associated with the project.
+  # If no change logs are found, it sets the instance variable to `nil`.
   before_action :get_change_log, only: %i[ show edit update ]
 
   # GET /projects or /projects.json
+
+  # The `index` action is responsible for rendering the projects index view.
+  # It retrieves all the projects and assigns them to the `@projects` instance variable.
+  # It also retrieves the search parameters and assigns them to the `@q` instance variable.
+  # The `@q` variable is used to filter the projects based on the search parameters.
+  # It also retrieves the `active` parameter and filters the projects based on the `active` parameter.
+  # If the `active` parameter is not present, it retrieves all the projects.
+  # It also paginates the projects using the `paginate` method from the `will_paginate` gem.
+  # The `paginate` method takes in two arguments: `page` and `per_page`.
+  # The `page` argument is used to specify the page number of the paginated results.
+  # The `per_page` argument is used to specify the number of projects to be displayed per page.
+  # The `per_page` argument is set to `3` by default.
+  # The `index` action also renders the `index` view.
   def index
     @q = Project.ransack(params[:q])
 
@@ -21,20 +49,40 @@ class ProjectsController < ApplicationController
   end
 
   # GET /projects/1 or /projects/1.json
+
+  # The `show` action is responsible for rendering the projects show view.
+  # It retrieves the project and assigns it to the `@project` instance variable.
+  # It also renders the `show` view.
   def show
     redirect_to edit_project_path(@project)
   end
 
   # GET /projects/new
+
+  # The `new` action is responsible for rendering the projects new view.
+  # It initializes a new project and assigns it to the `@project` instance variable.
+  # It also renders the `new` view.
   def new
     @project = Project.new
   end
 
   # GET /projects/1/edit
+
+  # The `edit` action is responsible for rendering the projects edit view.
+  # It retrieves the project and assigns it to the `@project` instance variable.
+  # It also renders the `edit` view.
   def edit
   end
 
   # POST /projects or /projects.json
+
+  # The `create` action is responsible for creating a new project.
+  # It initializes a new project with the project parameters and assigns it to the `@project` instance variable.
+  # It then attempts to save the project to the database.
+  # If the project is successfully saved, it redirects to the projects show page with a notice.
+  # If the project is not successfully saved, it renders the `new` view with an unprocessable entity error.
+  # The `create` action also calls the `create_change_log` method to create a change log for the project.
+  # The `create_change_log` method is defined below.
   def create
     @project = Project.new(project_params)
 
@@ -53,6 +101,14 @@ class ProjectsController < ApplicationController
   end
 
   # PATCH/PUT /projects/1 or /projects/1.json
+
+  # The `update` action is responsible for updating an existing project.
+  # It retrieves the project and assigns it to the `@project` instance variable.
+  # It then attempts to update the project with the project parameters.
+  # If the project is successfully updated, it redirects to the projects show page with a notice.
+  # If the project is not successfully updated, it renders the `edit` view with an unprocessable entity error.
+  # The `update` action also calls the `register_change_log` method to register the change log for the project.
+  # The `register_change_log` method is defined below.
   def update
     if params[:project][:stage] == "finished"
       params[:project][:stage_status] = "delivered"
@@ -73,6 +129,15 @@ class ProjectsController < ApplicationController
   end
 
   # DELETE /projects/1 or /projects/1.json
+
+  # The `destroy` action is responsible for deleting an existing project.
+  # It retrieves the project and assigns it to the `@project` instance variable.
+  # It then attempts to delete the project.
+  # If the project is successfully deleted, it redirects to the projects index page with a notice.
+  # If the project is not successfully deleted, it redirects to the projects index page with an alert.
+  # The `destroy` action also checks if the project has associated minutes and activities.
+  # If the project has associated minutes and activities, it redirects to the projects index page with an alert.
+  # If the project does not have associated minutes and activities, it deletes the project.
   def destroy
     @project.destroy if !@project.has_associated_minutes_and_activities?
 
@@ -89,6 +154,10 @@ class ProjectsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
+    # The `set_project` method is used to set the `@project` instance variable.
+    # It finds the project based on the provided `id` parameter.
+    # If the project is not found, it redirects to the projects index page with an alert.
     def set_project
       @project = Project.find(params[:id]) rescue nil
 
@@ -97,11 +166,17 @@ class ProjectsController < ApplicationController
       end
     end
 
+    # The `set_admins` method is used to set the `@admins` and `@all_admins` instance variables.
+    # It finds all the users with the role of `admin` and `collaborator` respectively.
+    # If no users are found, it sets the instance variables to `nil`.
     def set_admins
       @admins = User.where(role: "admin", status: "active").order(fullname: :desc)
       @all_admins = User.where(role: "admin").order(fullname: :desc)
     end
 
+    # The `get_change_log` method is used to set the `@project_change_log` instance variable.
+    # It finds all the change logs associated with the project.
+    # If no change logs are found, it sets the instance variable to `nil`.
     def get_change_log
       @project_change_log = ChangeLog.where(table_id: @project.id, table_name: "project").order(created_at: :desc)
       if @project_change_log.empty? || @project_change_log.nil?
@@ -109,6 +184,16 @@ class ProjectsController < ApplicationController
       end
     end
 
+    # The `register_change_log` method is used to register the change log for the project.
+    # It retrieves the changes made to the project and creates a change log for each change.
+    # It also sets the description for each change log.
+    # The `register_change_log` method is called in the `update` action.
+
+    # It sets the `table_id`, `user_id`, `description`, and `table_name` parameters.
+    # The `table_id` parameter is set to the project id.
+    # The `user_id` parameter is set to the current user id.
+    # The `description` parameter is set to the description of the change log.
+    # The `table_name` parameter is set to `project`.
     def register_change_log
       changes = @project.previous_changes
 
@@ -149,7 +234,6 @@ class ProjectsController < ApplicationController
         count += 1
       end
 
-      # Construir el registro del cambio
       return if description.empty?
 
       author = current_user.get_short_name
@@ -159,6 +243,9 @@ class ProjectsController < ApplicationController
       description = ""
     end
 
+    # The `create_change_log` method is used to create a change log for the project.
+    # It sets the description for the change log.
+    # The `create_change_log` method is called in the `create` action.
     def create_change_log
       description = "[#{Time.now.strftime("%d/%m/%Y - %H:%M")}] #{current_user.get_short_name} crea este proyecto."
       ChangeLog.new(table_id: @project.id, user_id: current_user.id, description: description, table_name: "project").save
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
+    # Enums define the possible values for the `stage` and `stage_status` attributes.
+    # The `stage` attribute can only be one of the following values:
     enum :stage, { 
         initial_meetings: 0, 
         preliminary_studies: 1, 
@@ -17,36 +19,47 @@ class Project < ApplicationRecord
         delivered: 2
     }
 
-    #validations
+    # The `before_save` callback method ensures that the `name` attribute is always
     before_save { self.name = name.upcase }
+
+    # Before saving, trims leading and trailing whitespace from name and location attributes.
     before_save :trim_values
+
+    # Before destroying a project, it removes all associated changes from the `ChangeLog` model.
     before_destroy :clean_changes
 
+    # The `validates` method is used to validate the presence and length of the `name` and `location` attributes.
     validates :name, presence: true
     validates :name, length: { in: 2..100 }, if: -> { name.present? }
-
-    validates :start_date, presence: true
-    validates :scheduled_deadline, presence: true
 
     validates :location, presence: true
     validates :location, length: { in: 2..500 }, if: -> { location.present? }
     
+    # The `validates` method is used to validate the presence of the `start_date` and `scheduled_deadline` attributes.
+    validates :start_date, presence: true
+    validates :scheduled_deadline, presence: true
 
+    # The `validates` method is used to validate the presence of the `stage` and `stage_status` attributes.
     validates :stage, presence: true
     validates :stage_status, presence: true
+
+    # The validation below ensures that the `scheduled_deadline` attribute is greater than the `start_date` attribute.
     validate :deadline_greater_than_start_date
 
+    # The trim_values method is used to trim leading and trailing whitespace from the `name` and `location` attributes.
     def trim_values
         self.name = name.strip if name.present?
         self.location = location.strip if location.present?
     end
 
+    # The deadline_greater_than_start_date method is used to validate that the `scheduled_deadline` attribute is greater than the `start_date` attribute.
     def deadline_greater_than_start_date
         if start_date.present? && scheduled_deadline.present? && scheduled_deadline <= start_date
             errors.add(:scheduled_deadline, "debe ser mayor a la fecha de inicio")
         end
     end
 
+    # The get_stage_options method returns an array of arrays containing the humanized stage and its corresponding value.
     def get_stage_options
         stage_options = [
             ["Visitas y reuniones iniciales", :initial_meetings],
@@ -61,6 +74,7 @@ class Project < ApplicationRecord
         ]
     end
 
+    # The get_status_options method returns an array of arrays containing the humanized status and its corresponding value.
     def get_status_options
         status_options = [
             ["En proceso", :in_process],
@@ -69,6 +83,7 @@ class Project < ApplicationRecord
         ]
     end
 
+    # The get_humanize_stage method returns the humanized stage value for the current project.
     def get_humanize_stage(stage_value = nil)
         stage_value ||= self.stage
 
@@ -82,6 +97,7 @@ class Project < ApplicationRecord
         humanized_stage
     end
 
+    # The get_humanize_status method returns the humanized status value for the current project.
     def get_humanize_status(status_value = nil)
         status_value ||= self.stage_status
         status_options = self.get_status_options
@@ -93,25 +109,30 @@ class Project < ApplicationRecord
         @status
     end
 
+    # The has_associated_minutes_and_activities? method returns true if the current project has associated minutes or activities.
     def has_associated_minutes_and_activities?
         minutes = self.minutes.count
         activities = self.activities.count
         activities != 0 || minutes != 0
     end
 
+    # The self.ransackable_associations method is used to allow the `ransack` gem to search through the `activities`, `minutes`, and `user` associations.
     def self.ransackable_associations(auth_object = nil)
         ["activities", "minutes", "user"]
     end
 
+    # The self.ransackable_attributes method is used to allow the `ransack` gem to search through the `name`, `stage`, `stage_status`, and `user_id` attributes.
     def self.ransackable_attributes(auth_object = nil)
         ["name", "stage", "stage_status", "user_id"]
     end
 
+    # The self.ransortable_attributes method is used to allow the `ransack` gem to sort through the `name`, `stage`, `stage_status`, and `user_id` attributes.
     def clean_changes
         ChangeLog.where(table_id: self.id, table_name: "project").destroy_all
     end
 
-    #associations
+    # Associations between the `Project`, `Activity`, `Minute`, and `User` models.
+    # A project `has_many` activities and minutes, and `belongs_to` a user.
     belongs_to :user
     has_many :activities, dependent: :destroy
     has_many :minutes, dependent: :destroy
